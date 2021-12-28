e71b6c053bc3c507fbe851ccaaadad18858a2889
Agustin Sly <agustin_sly@hotmail.com>
2021-12-28 16:33:22 -0300

    Update App.tsx
    - Pruebas modificando los componentes de react, agregando campos, botones y textinput

--- App.tsx
+++ App.tsx
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import React, { useState } from 'react'
+
+let textGlobalVariable = <Text>Im a global text variable inside the app</Text>
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	let textLocalVariable = <Text>Im a local text variable inside the app</Text>
+	const [text, setText] = useState('')
+	
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.tsx to start working on your app!</Text>
+			{textGlobalVariable}
+			{textLocalVariable}
+
+			<TextInput
+					value={text}
+					style={{ fontSize: 42, color: 'steelblue' }}
+					placeholder="Type here..."
+					onChangeText={(text) => {
+					setText(text)
+					}}
+			/>
+			<Text style={{ fontSize: 24 }}>
+				{'\n'}You entered: {text}
+			</Text>
+
+			<Button
+              onPress={() => alert('This is a button! The text entered on the field is: '+text)}
+              title="Press me!"
+              color="#1ACDA5"
+            />
+
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
