1b765ad32ab31b0ef79f241ea4c99b35c0c6bbcc
Agustin Sly <agustin_sly@hotmail.com>
2022-01-25 15:03:03 -0300

    Agregado un archivo con duplicados para probar

---
+++ Maps.tsx
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, TextInput, Image, FlatList } from 'react-native';
+import React, { useState } from 'react';
+
+const numbers = [1, 2, 3, 4, 5];
+
+function duplicateNumbers(toDuplicate: any){
+    let length = Object.keys(toDuplicate).length;
+    let numberKeys = Object.keys(toDuplicate);
+    let numbersTransformed = [];
+    for (let i = 0; i < length; i++){
+        numbersTransformed[i]=toDuplicate[numberKeys[i]]*2;
+    }
+	console.log("Duplicated numbers: "+numbersTransformed);
+    return numbersTransformed;
+}
+
+export default function App() {
+	return (
+		<View style={styles.container}>
+            <Text>Numeros antes de transformarlos: {numbers}</Text>
+            <Text>Numeros despues de transformarlos: {duplicateNumbers(numbers)}</Text>
+            <Button
+				onPress={() => duplicateNumbers(numbers)}
+				title="Transformar números"
+				color="#1ACDA5"
+			/>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	}
+});
